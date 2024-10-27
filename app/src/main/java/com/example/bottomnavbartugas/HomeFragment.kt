91c4ae4f2aeb0d517051e152c1ad01d6544dc93b
joes <joeswaruwu@gmail.com>
2024-10-27 15:37:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavbartugas/HomeFragment.kt
@@ -0,0 +1,44 @@
+package com.example.bottomnavbartugas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bottomnavbartugas.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        with(binding){
+//            val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment(textNama.text.toString())
+//            findNavController().navigate(action)
+////            findNavController().navigateUp()
+//        }
+//    }
+}
---
+++ app/src/main/java/com/example/bottomnavbartugas/ProfileFragment.kt
@@ -0,0 +1,49 @@
+package com.example.bottomnavbartugas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.bottomnavbartugas.databinding.FragmentProfileBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        with(binding) {
+//            btnToHome.setOnClickListener {
+//                findNavController().navigateUp()
+//            }
+//            val args: ProfileFragmentArgs by navArgs()
+//            txtname.text = args.userName
+//        }
+//
+//    }
+}
